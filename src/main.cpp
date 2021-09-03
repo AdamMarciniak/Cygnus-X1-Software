bcdda953753756a751c8cf256fc1696ecb3fcefa
Adam Marciniak <17841889+AdamMarciniak@users.noreply.github.com>
2021-09-02 23:50:52 -0700

    Fix rocket not transitioning to unpowered ascent state because accel magnitude from other az and ay prevent it

--- src/main.cpp
+++ src/main.cpp
@@ -291,7 +291,8 @@ void loop()
     }
 
     data.accelMag = sqrt(sq(data.ax) + sq(data.ay) + sq(data.az));
-    if (data.accelMag < ACCEL_UNPOWERED_THRESHOLD)
+    if (data.ax < ACCEL_UNPOWERED_THRESHOLD)
+      //Where accel unpowered threshold is less than -1.5
     {
       goToState(UNPOWERED_ASCENT);
     }
